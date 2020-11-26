6e7cc156dd2c83e6fc68432530a2577bbd589e4a
Bruna Scopel <brunascomor@gmail.com>
2020-11-26 12:53:45 +0100

    seeds ingredients to cocktail app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingr_serialized = open(url).read
+ingredients = JSON.parse(ingr_serialized)
+
+ingredients["drinks"].each do |pair|
+  Ingredient.create(name: pair["strIngredient1"])
+end
